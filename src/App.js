7ab028daa0f5e55b0dd6fd7d3b32e695ac62b985
amey-SN <ameynagmode@gmail.com>
2021-07-19 09:42:27 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from "./components/Navbar"
+import Layout from "./components/Layout";
+import Loader from "./components/Loader";
+import axios from 'axios';
+import React, { useState , useEffect} from 'react';
 
 function App() {
+  const [isClicked, setIsClicked] = useState(0);
+  const [data, setData] = useState(null);
+  const [isLoader, setIsLoader] = useState(false);
+  const fetchData = () => {
+    setIsLoader(true)
+    axios.get("https://reqres.in/api/users?page=1").then(
+      (response) => {
+        console.log("api data recieved")
+        setData(response.data.data);
+        setIsLoader(false)
+      }
+    )
+  }
+  useEffect(() => {
+    isClicked && fetchData();
+  }, [isClicked])
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
+     <>
+     <Navbar onSetIsClicked={setIsClicked}/>
+     <div className="row justify-content-center mb-2 mx-0">
+     {data&&data.map((val) => {
+       return  <Layout first_name={val.first_name} id={val.id} last_name={val.last_name} email={val.email} avatar={val.avatar}/>
+      })}
+    
+      </div>
+     <Loader show={isLoader} />
+     </>
+    
+  );}
 export default App;
